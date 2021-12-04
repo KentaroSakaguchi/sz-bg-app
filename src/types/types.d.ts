5366c1fa600410cf388dcc15f412a43749b8fb9f
KentarohSakaguchi <kentarohsakaguchi@gmail.com>
2021-12-04 20:51:25 +0900

    add: firebase

---
+++ src/types/types.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="@emotion/react/types/css-prop" />
+declare module '*.css';
+declare module '*.scss';
+declare module 'node-fetch';
+declare module 'draft-js';
+declare module '@editorjs/header';
+declare module '@editorjs/image';
+declare module '@editorjs/code';
+declare module '@editorjs/list';
+declare module '@editorjs/marker';
+declare module '@editorjs/inline-code';
+declare module 'validator/lib/isEmail';
